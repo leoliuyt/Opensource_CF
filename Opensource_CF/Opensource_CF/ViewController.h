0c09ae59b757f5a3167aa244778bc330d651907e
leoliuyt <leoliuyt@sina.cn>
2018-05-24 21:44:17 +0800

    create

---
+++ Opensource_CF/Opensource_CF/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Opensource_CF
+//
+//  Created by leoliu on 2018/5/24.
+//  Copyright © 2018年 leoliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Opensource_CF/Opensource_CF/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Opensource_CF
+//
+//  Created by leoliu on 2018/5/24.
+//  Copyright © 2018年 leoliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
